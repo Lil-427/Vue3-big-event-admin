c8676790747db70ffee8a024979eb83d0f9317e0
Lil-427 <fengli5311@163.com>
2026-05-10 20:38:18 +0800

    首次提交：测试一下

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,12 @@
 <script setup>
-import { useUserStore } from './stores/user'
+// import { useUserStore } from './stores/user'
 
-const userStore = useUserStore()
+// const userStore = useUserStore()
 </script>
 
 <template>
   <div>
     <router-view></router-view>
-    <hr />
-    <hr />
-    我是APP
-    <el-button>跳首页</el-button>
-    <el-button>跳列表页</el-button>
-
-    {{ userStore.token }}
-    <el-button @click="userStore.setToken('jksbds')">登录</el-button>
-    <el-button @click="userStore.removeToken">退出</el-button>
   </div>
 </template>
 
